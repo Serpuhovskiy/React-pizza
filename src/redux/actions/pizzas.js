6e375ae039f3b652638e3632c57e76a1c047cbae
Serpuhovskiy <sergey6335@mail.ru>
2022-03-23 23:35:38 +0300

    Edits

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// const API_URL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API : 'http://localhost:3003';
+const API_URL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API : 'http://localhost:3003';
 
-// console.log('NODE_ENV', process.env.NODE_ENV, 'REACT_APP_API', process.env.REACT_APP_API);
+console.log('NODE_ENV', process.env.NODE_ENV, 'REACT_APP_API', process.env.REACT_APP_API);
 
 export const setLoaded = (payload) => ({
   type: 'SET_LOADED',
@@ -15,14 +15,17 @@ export const fetchPizzas = (category, sortBy) => (dispatch) => {
   if (category !== null) {
     axios
       .get(
-        `${API_URL}/pizzas?category=${category}&_sort=${sortBy === 'alphabet' ? 'name' : sortBy
+        `/pizzas?category=${category}&_sort=${sortBy === 'alphabet' ? 'name' : sortBy
         }`)
+        // `${API_URL}/pizzas?category=${category}&_sort=${sortBy === 'alphabet' ? 'name' : sortBy
+        // }`)
       .then(({ data }) => {
         dispatch(setPizzas(data));
       });
   } else {
     axios
-      .get(`${API_URL}/pizzas?_sort=${sortBy === 'alphabet' ? 'name' : sortBy}`)
+      // .get(`${API_URL}/pizzas?_sort=${sortBy === 'alphabet' ? 'name' : sortBy}`)
+      .get(`/pizzas?_sort=${sortBy === 'alphabet' ? 'name' : sortBy}`)
       .then(({ data }) => {
         dispatch(setPizzas(data));
       });
